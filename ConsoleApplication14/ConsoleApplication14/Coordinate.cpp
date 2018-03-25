2c9604c46d80240db891b73d66b0cfd222d2c62e
Jiacheng Wei <982957368@qq.com>
2018-03-25 11:23:28 +0800

    2018.3.25

---
+++ ConsoleApplication14/ConsoleApplication14/Coordinate.cpp
@@ -0,0 +1,29 @@
+#include "stdafx.h"
+#include "Coordinate.h"
+
+Coordinate::Coordinate(int x,int y)
+{
+	m_iX = x;
+	m_iY = y;
+	cout << "Coordinate()" << endl;
+}
+Coordinate::~Coordinate()
+{
+	cout << "~Coordinate()" << endl;
+}
+void Coordinate::setX(int x)
+{
+	m_iX = x;
+}
+int Coordinate::getX()
+{
+	return m_iX;
+}
+void Coordinate::setY(int y)
+{
+	m_iY = y;
+}
+int Coordinate::getY()
+{
+	return m_iY;
+}
